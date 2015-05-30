23a76b7c3c501fc326dd97af78f88bbbeee74102
David Hunt <huntdog1541@gmail.com>
2015-05-30 06:17:56 -0500

    Create Context.java

---
+++ Context.java
@@ -0,0 +1,17 @@
+package vm;
+
+//To call, push one of these and pop the return
+public class Context {
+    Context invokingContext;        //parent int the stack or "caller"
+    FuncMetaData metadata;          //info about function we're executing
+    int returnip;
+    int[] locals;                   //args + locals, indexed from 0
+
+    public Context(Context invokingContext, int returnip, FuncMetaData metadata)
+    {
+        this.invokingContext = invokingContext;
+        this.returnip = returnip;
+        this.metadata = metadata;
+        locals = new int[metadata.nargs+metadata.nlocals];
+    }
+}
